ef669674c3fb2d2a5f46a723bed9473305165f62
milan473 <milangoh82@gmail.com>
2025-12-16 12:50:11 +0530

    Attach token to every request

--- jagger-client/src/api/api.js
+++ jagger-client/src/api/api.js
@@ -6,11 +6,20 @@ const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
-  // Attach token to every request
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
 });
+
+// Attach token to every request
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
