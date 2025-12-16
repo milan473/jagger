a9bb0cf398680945dc4b5e0fa8f38801d91507d4
milan473 <milangoh82@gmail.com>
2025-12-16 15:05:45 +0530

    api changes reverted

--- jagger-client/src/api/api.js
+++ jagger-client/src/api/api.js
@@ -1,25 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "/api/",
+  baseURL: "/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
 });
-
-// Attach token to every request
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
 
 export default api;
