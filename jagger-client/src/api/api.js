1b0b24f122d41438b308d141a5eeb85421fab18c
milan473 <milangoh82@gmail.com>
2025-12-16 14:00:55 +0530

    api.js  exp1

--- jagger-client/src/api/api.js
+++ jagger-client/src/api/api.js
@@ -2,6 +2,10 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "/api",
+  withCredentials: true,
+  header: {
+    "Content-Type":"applicatio/json",
+  }
 });
 
 // Attach token to every request
