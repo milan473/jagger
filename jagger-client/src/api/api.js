62bd8a98a4a35bd2af25423bef5d25e8244849d0
milan473 <milangoh82@gmail.com>
2025-12-17 13:34:46 +0530

    bearer token

--- jagger-client/src/api/api.js
+++ jagger-client/src/api/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
-    "Authorization": "Bearer your-secret-bearer-token-12345"
+    "Authorization": "Bearer ${token}",
   },
 });
 
