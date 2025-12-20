acc00cabd7ef16129530ed36cfef697421e90596
milan473 <76590095+milan473@users.noreply.github.com>
2025-12-20 15:47:10 +0530

    Update cron.js

--- jagger-backend/src/jobs/cron.js
+++ jagger-backend/src/jobs/cron.js
@@ -1,20 +1,39 @@
 const cron = require("node-cron");
+// RFQ Automationx
+const { autoCloseRFQs, rfqReminder } = require("../automations/rfqAutomation");
+
+//Inventory automation
 const {
-  autoCloseRFQs,
-  rfqReminder
-} = require("../automations/rfqAutomation");
+  checkLowInventory,
+  autoGenerateRFQ,
+} = require("../automations/inventoryAutomation");
+
+// Email automation
+const {
+  sendDailySummaryEmail,
+  sendPendingPOReminder,
+} = require("../automations/emailAutomation");
 
 // Runs every day at 12:00 AM
-// cron.schedule("0 0 * * *", async () => {
-//   console.log("⏰ Running RFQ automations...");
-//   await autoCloseRFQs();
-//   await rfqReminder();
-// });
+cron.schedule("0 0 * * *", async () => {
+  console.log("⏰ Running RFQ automations...");
+  await autoCloseRFQs();
+  await rfqReminder();
+  await checkLowInventory();
+  await autoGenerateRFQ();
+  await sendDailySummaryEmail();
+  await sendPendingPOReminder();
+});
+
 // cron.schedule("*/15 * * * * *", async () => {
 //   console.log("⏰ [TEST] Cron triggered at", new Date().toLocaleTimeString());
 
 //   await autoCloseRFQs();
 //   await rfqReminder();
+//   await checkLowInventory();
+//   await autoGenerateRFQ();
+//   await sendDailySummaryEmail();
+//   await sendPendingPOReminder();
 
 //   console.log("✅ [TEST] RFQ automation cycle finished");
 // });
